00e3df6e9c4a6287d022e139120b6b2e6d7c13db
harshit <harshit.py@gmail.com>
2014-06-24 11:30:30 +0530

    changes

--- centralization/urls.py
+++ centralization/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-from .views import test
+# from .views import test
 
 urlpatterns = patterns('',
     # Examples:
